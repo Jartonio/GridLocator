f0c936713c0a855e3651ed4cc9558a26f1979338
Antonio <antonio@asturmail.com>
2023-09-10 21:24:24 +0200

    10/09/2023

--- app/src/main/java/com/example/gridlocator/MainActivity.java
+++ app/src/main/java/com/example/gridlocator/MainActivity.java
@@ -16,14 +16,37 @@ public class MainActivity extends AppCompatActivity {
         double longitudInicial, latitudInicial, longitudFinal, latitudFinal, distancia;
         String grid;
 
-        longitudInicial = GenerarCoordenadasAleatorias.longitudAleatoria();
-        latitudInicial = GenerarCoordenadasAleatorias.latitudAleatoria();
+
+        latitudInicial=GenerarCoordenadasAleatorias.latitudAleatoria();
+        longitudInicial= GenerarCoordenadasAleatorias.longitudAleatoria();
 
         grid = CalcularGrid.grid(latitudInicial, longitudInicial);
 
         longitudFinal = CalcularCoordenadasDesdeGrid.longitud(grid);
         latitudFinal = CalcularCoordenadasDesdeGrid.latitud(grid);
         distancia=CalcularDistanciaEntreCoordenadas.distancia(latitudInicial,longitudInicial,latitudFinal,longitudFinal);
+
+
+        Log.d("prueba", "Coordenadas iniciales: " + latitudInicial+", "+ longitudInicial);
+        Log.d("prueba", "Coordenadas finales  : " + latitudFinal+", "+ longitudInicial);
+        Log.d("prueba", "distancia: "+ distancia);
+        Log.d("prueba", "Grid coordenadas: : "+ grid);
+
+
+        grid=CalcularGridCuadriculas.grid(latitudInicial,longitudInicial);
+
+        longitudFinal = CalcularCoordenadasDesdeGrid.longitud(grid);
+        latitudFinal = CalcularCoordenadasDesdeGrid.latitud(grid);
+        distancia=CalcularDistanciaEntreCoordenadas.distancia(latitudInicial,longitudInicial,latitudFinal,longitudFinal);
+
+        Log.d("prueba", "Coordenadas iniciales: " + latitudInicial+", "+ longitudInicial);
+        Log.d("prueba", "Coordenadas finales  : " + latitudFinal+", "+ longitudInicial);
+        Log.d("prueba", "distancia: "+ distancia);
+        Log.d("prueba", "Grid coordenadas: : "+ grid);
+
+
+        //Log.d("prueba", "Grid:  "+grid);
+
 
     }
 
