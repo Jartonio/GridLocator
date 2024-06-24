405f777a2b67684925c8554bfa2c732555d29f63
Antonio <antonio@asturmail.com>
2024-06-24 12:23:41 +0200

    Terminada la clase GridLocator

--- app/src/main/java/com/example/gridlocator/GridLocator.java
+++ app/src/main/java/com/example/gridlocator/GridLocator.java
@@ -3,9 +3,7 @@ package com.example.gridlocator;
 public class GridLocator {
 
     private double latitud, longuitud, desviacion;
-
     private String gridLocator;
-
 
     public GridLocator() {
     }
@@ -14,13 +12,12 @@ public class GridLocator {
         return gridLocator;
     }
 
-    public void setLatitudLonguitud(double latitud,double longuitud) {
+    public void setLatitudLonguitud(double latitud, double longuitud) {
         this.latitud = latitud;
         this.longuitud = longuitud;
         gridLocator = calcularGridLocator(latitud, longuitud);
         desviacion = metrosDesviacion();
     }
-
 
     public double getDesviacion() {
         return desviacion;
@@ -34,8 +31,8 @@ public class GridLocator {
         double longitud = longitudInicial + 180;
         double latitud = latitudInicial + 90;
 
-        double grados,totalCuadros,ultimo,diferenciaCuadros;
-        int cuadrosAnteriores, numeroGrid,divisiones;
+        double grados, totalCuadros, ultimo, diferenciaCuadros;
+        int cuadrosAnteriores, numeroGrid, divisiones;
 
         char[] grid = new char[10];
 
@@ -146,11 +143,8 @@ public class GridLocator {
         lo4 = (miGrid[4] - 'A') * 0.08333333;//0.083333;
         lo6 = (miGrid[6] - '0') * 0.00833333;//0.0083333;
         lo8 = (miGrid[8] - 'A') * 0.00034722;//0.000347221;
-        //lo10 = (miGrid[10] - '0') * 0.0000347221; --  Activar si el grid tiene 6 de precision
-
         longitud = (lo0 + lo2 + lo4 + lo6 + lo8) - 180;
 
-        //return  Double.parseDouble(String.format("%.6f", longitud));
         return longitud + (0.0003472222 / 2);
     }
 
@@ -166,11 +160,8 @@ public class GridLocator {
         la5 = (miGrid[5] - 'A') * 0.04166666;//0.0416665;
         la7 = (miGrid[7] - '0') * 0.00416666;//0.00416665;
         la9 = (miGrid[9] - 'A') * 0.00017361;//0.00017361;
-        //la11 = (miGrid[11] - '0') * 0.000017361;  --  Activar si el grid tiene 6 de precision
-
         latitud = (la1 + la3 + la5 + la7 + la9) - 90;
 
-        //return  Double.parseDouble(String.format("%.6f",latitud));
         return latitud + (0.0001736111 / 2);
     }
 
--- app/src/main/java/com/example/gridlocator/MainActivity.java
+++ app/src/main/java/com/example/gridlocator/MainActivity.java
@@ -12,12 +12,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        double longitudInicial, latitudInicial, longitudFinal, latitudFinal, distancia;
+        double longitudInicial, latitudInicial, distancia;
 
-        String grid;
+        String grid, tag = "Log.GridLocator";
 
         double sumaDistancias = 0;
-        double de0a5=0;
+        double de0a5 = 0;
         double de5a10 = 0;
         double de10a20 = 0;
         double de20a30 = 0;
@@ -25,66 +25,53 @@ public class MainActivity extends AppCompatActivity {
         double de40a50 = 0;
         double mas50 = 0;
 
-        long veces = 1000000;
+        long veces = 10000;
 
-        GridLocator miGridLocator=new GridLocator();
+        GridLocator miGridLocator = new GridLocator();
 
         for (int i = 1; i <= veces; i++) {
-
             latitudInicial = GenerarCoordenadasAleatorias.latitudAleatoria();
             longitudInicial = GenerarCoordenadasAleatorias.longitudAleatoria();
 
-            //grid = CalcularGrid.grid(latitudInicial, longitudInicial);
+            miGridLocator.setLatitudLonguitud(latitudInicial, longitudInicial);
 
-            miGridLocator.setLatitudLonguitud(latitudInicial,longitudInicial);
-
-            //longitudFinal = CalcularCoordenadasDesdeGrid.longitud(grid);
-            //latitudFinal = CalcularCoordenadasDesdeGrid.latitud(grid);
-
-            //distancia = CalcularDistanciaEntreCoordenadas.distancia(latitudInicial, longitudInicial, latitudFinal, longitudFinal);
-            distancia=miGridLocator.getDesviacion();
+            distancia = miGridLocator.getDesviacion();
             sumaDistancias = sumaDistancias + distancia;
 
             if (distancia <= 5) {
                 de0a5++;
-            }
-            if (distancia > 5 && distancia <= 10) {
-                de5a10++;
-            }
-
-            if (distancia > 10 && distancia <= 20) {
-                de10a20++;
-            }
-            if (distancia > 20 && distancia <= 30) {
-                de20a30++;
-            }
-            if (distancia > 30 && distancia <= 40) {
-                de30a40++;
-            }
-            if (distancia > 40 && distancia <= 50) {
-                de40a50++;
-            }
-            if (distancia > 50) {
-                mas50++;
+            } else {
+                if (distancia > 5 && distancia <= 10) {
+                    de5a10++;
+                } else {
+                    if (distancia > 10 && distancia <= 20) {
+                        de10a20++;
+                    } else {
+                        if (distancia > 20 && distancia <= 30) {
+                            de20a30++;
+                        } else {
+                            if (distancia > 30 && distancia <= 40) {
+                                de30a40++;
+                            } else {
+                                if (distancia > 40 && distancia <= 50) {
+                                    de40a50++;
+                                } else {
+                                    mas50++;
+                                }
+                            }
+                        }
+                    }
+                }
             }
         }
-/*
-            Log.d("prueba", "Coordenadas iniciales: " + latitudInicial + ", " + longitudInicial);
-            Log.d("prueba", "Coordenadas finales  : " + latitudFinal + ", " + longitudFinal);
-            Log.d("prueba", "distancia: " + distancia);
-            Log.d("prueba", "Grid coordenadas: : " + grid);
 
-
-
-*/
-            Log.d("prueba", "Media de error: " + (sumaDistancias / veces));
-            Log.d("prueba", "Distancias menores de 5m:   " + de0a5);
-            Log.d("prueba", "Distancias entre 5m y 10m:  " + de5a10);
-            Log.d("prueba", "Distancias entre 10m y 20m: " + de10a20);
-            Log.d("prueba", "Distancias entre 20m y 30m: " + de20a30);
-            Log.d("prueba", "Distancias entre 30m y 40m: " + de30a40);
-            Log.d("prueba", "Distancias entre 40m y 50m: " + de40a50);
-            Log.d("prueba", "Distancias de mas de 50m:   " + mas50);
-
+        Log.d(tag, "Media de error: " + (sumaDistancias / veces));
+        Log.d(tag, "Distancias menores de 5m:   " + de0a5);
+        Log.d(tag, "Distancias entre 5m y 10m:  " + de5a10);
+        Log.d(tag, "Distancias entre 10m y 20m: " + de10a20);
+        Log.d(tag, "Distancias entre 20m y 30m: " + de20a30);
+        Log.d(tag, "Distancias entre 30m y 40m: " + de30a40);
+        Log.d(tag, "Distancias entre 40m y 50m: " + de40a50);
+        Log.d(tag, "Distancias de mas de 50m:   " + mas50);
     }
 }
